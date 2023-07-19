85b5a24e0a3442d6a70cda2221f3efe34065d416
hihijin <117073214+hihijin@users.noreply.github.com>
2023-07-19 22:56:37 +0900

    [Feat]: SearchContainer 구현

---
+++ src/components/SearchContainer.tsx
@@ -0,0 +1,94 @@
+import '../Global.css';
+
+import React from 'react';
+
+import { AiOutlineSearch } from 'react-icons/ai';
+import styled from 'styled-components';
+
+const Ul = styled.ul`
+	z-index: 300;
+	width: 100%;
+	div {
+		color: rgba(0, 0, 0, 0.5);
+		margin-left: 15px;
+		font-size: 12px;
+	}
+`;
+
+const Li = styled.li`
+	width: inherit;
+	padding: 10px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 300;
+
+	> button {
+		width: 100%;
+		display: flex;
+		&:hover {
+			color: #0db4f3;
+		}
+
+		> p {
+			height: 18px;
+			font-size: 13px;
+			margin-left: 15px;
+			display: flex;
+		}
+	}
+`;
+
+const Container = styled.div`
+	background: white;
+	border-radius: 20px;
+	position: absolute;
+	top: 70px;
+	padding: 15px 10px;
+	z-index: 300;
+	width: 100%;
+	left: 0px;
+	overflow: scroll;
+	&::-webkit-scrollbar {
+		display: none;
+	}
+`;
+
+type Iprops = {
+	searchResult: string[];
+	setSearchResult: React.Dispatch<React.SetStateAction<string[]>>;
+	setValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function SearchContainer({
+	searchResult,
+	setSearchResult,
+	setValue,
+}: Iprops) {
+	const keywordSelected = (selectedOption: string) => {
+		setValue(selectedOption);
+		setSearchResult([]);
+		window.location.assign(
+			`https://clinicaltrialskorea.com/studies?conditions=${selectedOption}`,
+		);
+	};
+	return (
+		<Container>
+			<Ul>
+				<div>추천 검색어</div>
+				{searchResult.length > 0 ? (
+					searchResult.map((result: string) => (
+						<Li key={result}>
+							<AiOutlineSearch size="20px" color="rgba(0, 0, 0, 0.8)" />
+							<button onClick={() => keywordSelected(result)}>
+								<p>{result}</p>
+							</button>
+						</Li>
+					))
+				) : (
+					<Li>검색어 없음</Li>
+				)}
+			</Ul>
+		</Container>
+	);
+}
